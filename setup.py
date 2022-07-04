b5eedff3e72c3eab1799fa83ced07aaed17f406b
Hiroyuki Ohsaki <ohsaki@lsnl.jp>
2022-07-05 02:03:30 +0900

    version 1.6

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='rplot',
-    version='1.5',
+    version='1.6',
     author='Hiroyuki Ohsaki',
     author_email='ohsaki@lsnl.jp',
     description='A stream-based realtime scientific data plotter',
